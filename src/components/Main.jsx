6d36e5933ecaa6744e5a8d89b8c1b9568c8f2e7f
milosbunijevac <milos.bunijevac@gmail.com>
2017-08-16 18:00:41 -0400

    "Input fields and text areas now change the state as they should"

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
+import EmailField from './EmailField.jsx';
+import InputTextField from './InputTextField.jsx';
+
 class Main extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    this.state = {recEmail: '', bodyText: ''};
+    this.emailFieldChange = this.emailFieldChange.bind(this);
+    this.inputTextChange = this.inputTextChange.bind(this);
+  }
+
+  emailFieldChange (e) {
+    this.setState({recEmail: e.target.value});
+  }
+
+  inputTextChange (e) {
+    this.setState({bodyText: e.target.value});
   }
 
   render() {
     return (
       <div>
-        SEE ME?
+        <div className = "emailFields">
+          <EmailField emailInput={this.emailFieldChange}/>
+          <InputTextField textAreaInput={this.inputTextChange}/>
+        </div>
       </div>
     );
   }
